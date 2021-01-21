314ae23e46a7f0688f75ecc0aed0f9ac36abbf84
rovin0805 <qhdrn67@naver.com>
2021-01-21 18:49:01 +0900

    asked for permissions

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
   getLoctaion = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you", "so saaad");
+    }
   };
 
   componentDidMount() {
     this.getLoctaion();
   }
 
-  reder() {
-    return <Loading />;
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
